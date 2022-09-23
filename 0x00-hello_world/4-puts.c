211cd07c5977fb4c164ad3e45e3eba0cde647f7a
Nuria Abdu <nuriaabdu9@gmail.com>
2022-09-23 10:44:09 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include  <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: C program that print function followed by a new line using put
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
